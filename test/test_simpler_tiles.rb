79a937a15e37b3e2cf98ce6b00761c032b2928e9
Al Shaw <almshaw@gmail.com>
2014-07-21 16:20:31 -0400

    docs

--- test/test_simpler_tiles.rb
+++ test/test_simpler_tiles.rb
@@ -3,19 +3,20 @@ require "#{File.join(File.dirname(__FILE__))}/helper"
 class TestSimplerTiles < Test::Unit::TestCase
   should "produce a png image" do
     map = SimplerTiles::Map.new do |m|
-      # m.bgcolor = "#00000000"
+      # m.bgcolor = "#000000"
       m.srs = "EPSG:32614"
       m.width = 256
       m.height = 256
       m.set_bounds(195585.000, 3472515.000, 427815.000, 3235485.000)
-
       m.layer "#{File.dirname(__FILE__)}/../data/tl_2010_us_state10.shp" do |l|
         l.query "SELECT * from 'tl_2010_us_state10'" do |q|
-          q.styles 'fill' => "#ffffff",
+          q.styles 'fill' => "#000dff",
               'line-join' => "round",
                'line-cap' => "square"
         end
       end
+      # to set a nodata flag on a landsat look img
+      # gdal_translate -a_nodata 0 someplaceintexas.jpg someplaceintexas_nodata.jpg
       m.raster_layer "#{File.dirname(__FILE__)}/../data/someplaceintexas-nodata.jpg"
     end
 
